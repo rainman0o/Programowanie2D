3c79894d9d6aad30ce60b8c17b0a707a79b857bb
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-22 11:50:55 +0200

    xd

--- Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
+++ Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -5,5 +5,13 @@
 int main()
 {
     std::cout << "Hello World!\n" ;
+    std::cout << -59.5 << "dasdasda" << 'x' << true << false << '\n';
+
+    int number;
+    number = 5;
+
+    int secondNumber;
+    secondNumber = number + 3;
+
 }
 
