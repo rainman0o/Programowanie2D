ff14ace46ddede75c5dd0d775e076dca71fee6bc
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-21 16:48:26 +0100

    sad

--- Programowanie/StringConsoleApplication/StringConsoleApplication.cpp
+++ Programowanie/StringConsoleApplication/StringConsoleApplication.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 using namespace std;
 
+//Napisz program który wczyta znak z klawiatury i sprawdzi czu jest to mala litera alfabetu
+void task1()
+{
+	char signFromUser;
+	//signFromUser = 'a';
+	,0
+	cout << "podaj znak\n";
+	cin >> signFromUser;
+
+	cout << "Podales znak: " << signFromUser << "/n";
+
+
+	//if (signFromUser >= 97 && signFromUser <= 122)
+	if (signFromUser >= 'a' && signFromUser <= 'z')
+	{
+		cout << "Znak jest mala litera alfabetu";
+	}
+	else
+	{
+		cout << "Znak nie jest mala litera alfabetu";
+	}
+
+	signFromUser = signFromUser + 2;
+	cout << "Podales znak" << signFromUser;
+
+	signFromUser = signFromUser + 'a';
+	cout << "Podales znak" << signFromUser;
+
+}
+
 int main()
 {
-   
+	task1();
 }
 
