5b08794ca0a68157994f728c84dead8ad6f974b7
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-01 13:06:30 +0100

    fds

--- Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -9,24 +9,64 @@ private:
     int z;
 
 public:
+    Point()
+    {
+        x = 3;
+        y = 7;
+        z = 10;
+    }
+
+    Point(int xx)
+    {
+        x = xx;
+        y = 7;
+        z = 10;
+    }
+
+    Point(int xx, int yy, int zz )
+    {
+        x = xx;
+        y = yy;
+        z = zz;
+    }
+
+    void SetX(int value)
+    {
+        //x=abs(value)
+
+        if (value >= 0)
+        {
+            x = value;
+        }
+        else
+        {
+            //reakcja na b³¹d
+        }
+    }
+    int GetX()
+        {
+            //if(uprawnienia)
+            return x;
+        }
+    
+    double distanceFromCenter(Point p)
+    {
+        double distance;
+        distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+        return distance;
+    }
 };
 
-double distanceFromCenter(Point p)
-{
-    double distance;
-    distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
-    return distance;
-}
+
 
 int main()
 {
     setlocale(LC_CTYPE, "polish"); 
 
     Point firstPoint;
-    firstPoint.x = 3;
-    firstPoint.y = 5;
-    firstPoint.z = 8;
+   // firstPoint.x = 3;
+   // firstPoint.y = 5;
+   //firstPoint.z = 8;
 
-    cout << "Odleg³oœæ " << distanceFromCenter(firstPoint) << "\n";
+    cout << "Odleg³oœæ " << firstPoint.distanceFromCenter(firstPoint) << "\n";
 }
-
