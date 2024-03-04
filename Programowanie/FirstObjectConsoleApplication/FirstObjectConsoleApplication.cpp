8174dc91cd5eeb1e145196a32d55e8889bf97040
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-04 11:32:49 +0100

    das

--- Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -1,72 +1,138 @@
-#include <iostream>
-using namespace std;
+﻿#include <iostream>
 
 class Point
 {
 private:
-    int x;
-    int y;
-    int z;
+	int x;
+	int y;
+	int z;
 
 public:
-    Point()
-    {
-        x = 3;
-        y = 7;
-        z = 10;
-    }
+	Point()
+	{
+		x = 3;
+		y = 7;
+		z = 10;
+	}
 
-    Point(int xx)
-    {
-        x = xx;
-        y = 7;
-        z = 10;
-    }
+	Point(int xx)
+	{
+		x = xx;
+		y = 7;
+		z = 10;
+	}
 
-    Point(int xx, int yy, int zz )
-    {
-        x = xx;
-        y = yy;
-        z = zz;
-    }
+	~Point()
+	{
 
-    void SetX(int value)
-    {
-        //x=abs(value)
+	}
 
-        if (value >= 0)
-        {
-            x = value;
-        }
-        else
-        {
-            //reakcja na b��d
-        }
-    }
-    int GetX()
-        {
-            //if(uprawnienia)
-            return x;
-        }
-    
-    double distanceFromCenter(Point p)
-    {
-        double distance;
-        distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
-        return distance;
-    }
+	Point(int xx, int yy, int zz)
+	{
+		x = xx;
+		y = yy;
+		z = zz;
+	}
+
+	void SetX(int value)
+	{
+		//x = abs(value);
+		if (value >= 0)
+			x = value;
+		else
+		{
+			//reakcja na błąd
+		}
+	}
+
+	int GetX()
+	{
+		//if (uprawnienia)
+
+		return x;
+	}
+
+	double DistanceFromCenter()
+	{
+		double distance;
+		distance = sqrt(x * x + y * y + z * z);
+		return distance;
+	}
+};
+
+class Rectrangle
+{
+private:
+	int firstSide;
+	int secondSide;
+	int thirdSide;
+	int fourthSide;
+	std::string whichRectrangle;
+
+public:
+	
+	Rectrangle()
+	{
+		firstSide = 2;
+		secondSide = 2;
+		thirdSide = 4;
+		fourthSide = 4;
+		whichRectrangle = "Pierszy prostokąt";
+	}
+
+	Rectrangle(int side1, int side2, std::string name)
+	{
+		firstSide = secondSide = side1;
+		thirdSide = fourthSide = side2;
+		whichRectrangle = name;
+	}
+
+	~Rectrangle()
+	{
+
+	}
+
+	int AreaOfRectrangle()
+	{
+		int area;
+		area = firstSide * thirdSide;
+		return area;
+	}
+	
+	int CircutOfRectrangle()
+	{
+		int circut;
+		circut = firstSide + secondSide + thirdSide + fourthSide;
+		return circut;
+	}
+
+	void InformationAboutRectrangle()
+	{
+		std::cout << whichRectrangle << "\n";
+		std::cout << "obwód: " << CircutOfRectrangle()<< "\n";
+		std::cout << "pole: " << AreaOfRectrangle() << "\n";
+	}
+
 };
 
 
 
 int main()
 {
-    setlocale(LC_CTYPE, "polish"); 
+	//Point firstPoint;
+	//firstPoint.x = 3;
+	//firstPoint.SetX(-3);
+	//firstPoint.y = 5;
+	//firstPoint.z = 8;
+	//std::cout << "Odległość " << firstPoint.DistanceFromCenter() << "\n";
+	//Point secondPiont(15);
+	//std::cout << "Odległość " << secondPiont.DistanceFromCenter() << "\n";
 
-    Point firstPoint;
-   // firstPoint.x = 3;
-   // firstPoint.y = 5;
-   //firstPoint.z = 8;
+	Rectrangle firstRectrangle;
+	Rectrangle secondRectrangle(5, 7, "drugi Prostokąt");
 
-    cout << "Odleg�o�� " << firstPoint.distanceFromCenter(firstPoint) << "\n";
+	
+   firstRectrangle.InformationAboutRectrangle();
+   std::cout << "\n";
+   secondRectrangle.InformationAboutRectrangle();
 }
