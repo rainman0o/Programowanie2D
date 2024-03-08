de995fafe938d2bce66ffd7bb4cd8c181eedcfa2
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-08 13:17:45 +0100

    dsadsddsd

--- Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -70,7 +70,7 @@ private:
 	std::string whichRectrangle;
 
 public:
-	
+
 	Rectrangle()
 	{
 		firstSide = 2;
@@ -98,7 +98,7 @@ public:
 		area = firstSide * thirdSide;
 		return area;
 	}
-	
+
 	int CircutOfRectrangle()
 	{
 		int circut;
@@ -109,8 +109,78 @@ public:
 	void InformationAboutRectrangle()
 	{
 		std::cout << whichRectrangle << "\n";
-		std::cout << "obwód: " << CircutOfRectrangle()<< "\n";
+		std::cout << "obwód: " << CircutOfRectrangle() << "\n";
 		std::cout << "pole: " << AreaOfRectrangle() << "\n";
+	}
+
+};
+
+/*
+ZADANIE
+Zdefiniuj klase opisujące konto bankowe. Możliwości klasy:
+* konstruktory
+* metoda która dokona wpłaty na konto
+* metoda która wypłaty z konta
+* metoda która wykona przelew na inne konto
+* metodę która pokaże informacje o koncie.
+
+Napisz program który zaprezentuje możliwości obiektu na podstawie tej klasy.
+*/
+
+class  Account
+{
+private:
+	float accountBalance;
+
+public:
+
+	Account(int money)
+	{
+		accountBalance = money;
+	}
+
+	~Account()
+	{
+
+	}
+
+	int Payment(int howMuchMoney)
+	{
+		accountBalance = accountBalance + howMuchMoney;
+		return accountBalance;
+	}
+
+
+	bool Paycheck(int howMuchMoney)
+	{	
+		if (howMuchMoney<= accountBalance)
+		{
+			accountBalance = accountBalance - howMuchMoney;
+			return true;
+		}
+		return false;
+	}
+
+	void AccountInformation()
+	{
+		if (accountBalance > 0)
+		{
+			std::cout << "aktualny stan konta wynosi: " << accountBalance;
+		}
+		else
+		{
+			std::cout << " ";
+		}
+	}
+
+	bool Transfer(int howMuch, Account &target)
+	{
+		if (Paycheck(howMuch))
+		{
+			target.Payment(howMuch);
+			return true;
+		}	
+		return false;
 	}
 
 };
@@ -118,7 +188,10 @@ public:
 
 
 int main()
+
 {
+	setlocale(LC_CTYPE, "polish");
+
 	//Point firstPoint;
 	//firstPoint.x = 3;
 	//firstPoint.SetX(-3);
@@ -128,11 +201,55 @@ int main()
 	//Point secondPiont(15);
 	//std::cout << "Odległość " << secondPiont.DistanceFromCenter() << "\n";
 
-	Rectrangle firstRectrangle;
-	Rectrangle secondRectrangle(5, 7, "drugi Prostokąt");
+	//Rectrangle firstRectrangle;
+	//Rectrangle secondRectrangle(5, 7, "drugi Prostokąt");
 
-	
-   firstRectrangle.InformationAboutRectrangle();
-   std::cout << "\n";
-   secondRectrangle.InformationAboutRectrangle();
+
+	//firstRectrangle.InformationAboutRectrangle();
+	//std::cout << "\n";
+	//secondRectrangle.InformationAboutRectrangle();
+
+	Account konto1(20);
+	Account konto2(10);
+
+	//informacje o stanie konta
+	konto1.AccountInformation();
+	std::cout << "\n";
+	konto2.AccountInformation();
+
+	std::cout << "\n" << "TEST METODY WPLACANIA" << "\n";
+
+	//wpłata na konto bankowe
+	std::cout << "\n";
+	konto1.Payment(4000);
+	konto1.AccountInformation();
+	std::cout << "\n";
+	konto2.Payment(789);
+	konto2.AccountInformation();
+
+	std::cout << "\n" << "TEST METODY WYPLACANIA" << "\n";
+
+	//wypłata z konta bankowego
+	std::cout << "\n";
+	konto1.Paycheck(1900);
+	konto1.AccountInformation();
+	std::cout << "\n";
+	konto2.Paycheck(763);
+	konto2.AccountInformation();
+
+	std::cout << "\n" << "TEST PRZELEWU NA KONTO" << "\n";
+
+	//przelew z jednego konta na drugie
+	std::cout << "\n";
+	if (konto1.Transfer(23, konto2))
+	{
+		std::cout << "przelew się udał";
+	}
+	else
+	{
+		std::cout << "przelew się nie udał";
+	}
+	konto1.AccountInformation();
+	std::cout << "\n";
+	konto2.AccountInformation();
 }
