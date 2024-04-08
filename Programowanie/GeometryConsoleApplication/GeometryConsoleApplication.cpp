2284c6ba06a4961ae43d9f08298abf4c09e9afb0
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-04-08 11:36:42 +0200

    weq

--- Programowanie/GeometryConsoleApplication/GeometryConsoleApplication.cpp
+++ Programowanie/GeometryConsoleApplication/GeometryConsoleApplication.cpp
@@ -7,17 +7,38 @@ class Quadrangle
 protected:
     double sideA, sideB, sideC, sideD, height;
 public:
-
+    Quadrangle(double firstSide,
+        double secondSide,
+        double thirdSide,
+        double fourthSide,
+        double h)
+    {
+        sideA = firstSide;
+        sideB = secondSide;
+        sideC = thirdSide;
+        sideD = fourthSide;
+        height = h;
+    }
+    
+    double GetPerimeter()
+    {
+        return sideA + sideB + sideC + sideD;
+    }
 };
 
-class Square: public Quadrangle
+class Square: public Rectangle
 {
 protected:
 
 public:
-    Square(double side)
+    Square(double side):Rectangle(side, side)
     {
-        height = sideA = sideB = sideC = sideD = side;
+        //height = sideA = sideB = sideC = sideD = side;
+    }
+
+    double GetArea()
+    {
+        return sideA * sideB;
     }
 };
 
@@ -26,10 +47,15 @@ class Rectangle: public Quadrangle
 protected:
 
 public:
-    Rectangle(double firstSide, double secondSide)
+    Rectangle(double firstSide, double secondSide): Quadrangle(firstSide, secondSide, firstSide, secondSide, secondSide)
     {
-        sideA = sideC = firstSide;
-        height= sideB = sideD = secondSide;
+       /* sideA = sideC = firstSide;
+        height= sideB = sideD = secondSide;*/
+    }
+
+    double GetArea()
+    {
+        return sideA * sideB;
     }
 };
 
@@ -38,18 +64,28 @@ class Trapeze: public Quadrangle
 protected:
 
 public:
-    Trapeze(double firstSide, double secondSide, double thirdSide, double fourthSide, double h)
+    Trapeze(double firstSide,
+        double secondSide,
+        double thirdSide,
+        double fourthSide,
+        double h) : Quadrangle(firstSide, secondSide, thirdSide, fourthSide, h)
     {
-        sideA = firstSide;
+       /* sideA = firstSide;
         sideB = secondSide;
         sideC = thirdSide;
         sideD = fourthSide;
-        height = h;
+        height = h;*/
+    }
+
+    double GetArea()
+    {
+      return  (sideA + sideC)* height / 2;
     }
 };
 
 int main()
 {
+    Quadrangle q(3, 2, 4, 5, 2);
     Square s(6);
     Rectangle r(10,5);
     Trapeze t(10, 6, 4, 5, 3);
