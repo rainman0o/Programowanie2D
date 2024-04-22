f49cbdcf870e0977c95a510f6bf69b9995d9da5b
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-04-22 10:17:12 +0200

    xc

--- Programowanie/GeometryConsoleApplication/GeometryConsoleApplication.cpp
+++ Programowanie/GeometryConsoleApplication/GeometryConsoleApplication.cpp
@@ -83,6 +83,194 @@ public:
     }
 };
 
+
+/*
+
+* Dla poni¿szych klas u³ó¿ drzewko dziedziczenia.
+- , , , , , , , 
+- , , , , , , 
+- , , , , , 
+, , 
+*/
+
+//zad 1
+
+class Animal //Animal(Zwierzê)
+{
+
+};
+
+//rodzaje
+class Mamal :public Animal //Mammal (Ssak)
+{
+
+};
+
+class Fish :public Animal //Fish (Ryba)
+{
+
+};
+
+class Reptitle :public Animal //Reptile(Gad)
+{
+
+};        
+
+class Bird :public Animal //Bird(Ptak)
+{
+
+};
+
+
+//ptak
+class Eagle :public Bird  //Eagle(Orze³)
+{
+
+};
+
+class Sparrow :public Bird //Sparrow (Wróbel)
+{
+
+};
+
+
+//ssak
+class Tiger :public Mamal //Tiger(Tygrys)
+{
+
+};
+
+class Dog :public Mamal //Dog (Pies)
+{
+
+};
+
+class Cat :public Mamal //Cat (Kot)
+{
+
+};
+
+class Lion :public Mamal //Lion(Lew)
+{
+
+};
+
+//ryby
+
+class Salomon :public Fish //Salmon (£osoœ)
+{
+
+};
+
+class Trout :public Fish //Trout(Pstr¹g)
+{
+
+};
+
+//gady
+class Snake :public Reptitle //Snake (W¹¿)
+{
+
+};
+
+class Turtle :public Reptitle //Turtle (¯ó³w)
+{
+
+};
+
+
+//zad2
+
+class Vehicle //Vehicle(Pojazd)
+{
+
+};
+
+class Motorcycle :public Vehicle  //Motorcycle (Motocykl)
+{
+
+};
+
+class Car :public Vehicle //Car(Samochód)
+{
+
+};
+
+class Truck :public Vehicle //Truck (Ciê¿arówka)
+{
+
+};
+
+//samochód
+class Sedan :public Car //Sedan (Sedan)
+{
+
+};
+
+class SUV :public Car //SUV (SUV)
+{
+
+};
+
+class Pick_up :public Car //Pickup Truck (Pick-up)
+{
+
+};
+
+//ciezarówka
+
+class Semi_truck :public Truck //Semi-Truck (Ci¹gnik siod³owy)
+{
+
+};
+
+//zad3
+
+class Electronic_Device //Electronic Device(Urz¹dzenie elektroniczne)
+{
+
+};
+//urzadenia
+
+class Television :public Electronic_Device //Television (Telewizor)
+{
+
+};
+
+class Smartphone :public Electronic_Device //Smartphone(Smartfon)
+{
+
+}; 
+
+class Laptop :public Electronic_Device //Laptop(Laptop)
+{
+
+};
+
+//smartfony
+
+class Iphone :public Smartphone //iPhone (iPhone)
+{
+
+};
+
+class Android_device :public Smartphone //Android Phone (Telefon z systemem Android)
+{
+
+};
+
+//Telewiozry
+
+class LED_TV :public Television //LED TV (Telewizor LED)
+{
+
+};
+
+class Smart_TV :public Television //Smart TV (Telewizor Smart)
+{
+
+};
+
 int main()
 {
     Quadrangle q(3, 2, 4, 5, 2);
