b8a6b888ae6f9cc9e8943fffa7d7ef6e409d08ec
q <q>
2024-09-27 11:38:16 +0200

    dsa

--- Programowanie/StructConsoleApp/Program.cs
+++ Programowanie/StructConsoleApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Point firstPoint = new Point();
+firstPoint.x = 5;
+firstPoint.y = 10;
+Point secondPoint = new Point();
+secondPoint = firstPoint;
+secondPoint.x = 6;
+secondPoint.y = 99;
+Console.WriteLine(firstPoint.x);                                                                            
+Console.WriteLine(secondPoint.x);  
