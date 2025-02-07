4e138f9b3437077e30bc6406652f1153c06205cf
q <q>
2025-02-07 11:32:57 +0100

    4.2

--- Programowanie/MaturaCzerwiec2022ConsoleApp/Program.cs
+++ Programowanie/MaturaCzerwiec2022ConsoleApp/Program.cs
@@ -19,3 +19,13 @@ foreach(var strNumber in reversDividedBy17)
 {
     Console.WriteLine(strNumber);
 }
+
+Console.WriteLine("zadanie 4.2");
+
+var strNumberWithMaxDiffrence = strNumbers.OrderBy(s => Math.Abs(int.Parse(s)  - int.Parse(string.Join("", s.Reverse())))).LastOrDefault();
+
+Console.Write(strNumberWithMaxDiffrence);
+Console.Write(" ");
+Console.WriteLine(Math.Abs(int.Parse(strNumberWithMaxDiffrence) - int.Parse(string.Join("", strNumberWithMaxDiffrence.Reverse()))));
+
+Console.WriteLine("zadanie 4.3");
