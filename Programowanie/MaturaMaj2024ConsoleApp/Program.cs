8971f51a0e707432128b69f54c72257c0dfff9f3
q <q>
2025-04-04 10:40:55 +0200

    dsa

--- Programowanie/MaturaMaj2024ConsoleApp/Program.cs
+++ Programowanie/MaturaMaj2024ConsoleApp/Program.cs
@@ -8,12 +8,10 @@ while ((strNumberFromFile = streamReader.ReadLine()) != null)
     strNumbers.Add(strNumberFromFile);
 }
 
-streamReader.Close();
-
 List<int> intNumbers = strNumbers.Select(s => int.Parse(s)).ToList();
 
-
-int evenShortcut(int number)
+Console.WriteLine("zadanie 3.2");
+int oddShortcut(int number)
 {
     int result = 0;
     int multiplier = 1;
@@ -21,7 +19,7 @@ int evenShortcut(int number)
     while (number > 0)
     {
         int digit = number % 10;
-        if (digit % 2 == 0) 
+        if (digit % 2 != 0) 
         {
             result = digit * multiplier + result;
             multiplier *= 10;
@@ -30,5 +28,54 @@ int evenShortcut(int number)
     }
 
     return result;
+}
+int howManyNotOddShortcut = 0;
+int biggestNotOddShortcut = 0;
+
+foreach (int number in intNumbers)
+{
+   if(oddShortcut(number) == 0)
+    {
+        howManyNotOddShortcut++;
+        if(number > biggestNotOddShortcut)
+        {
+            biggestNotOddShortcut = number;
+        }
+    }
+}
+
+Console.WriteLine(howManyNotOddShortcut);
+Console.WriteLine(biggestNotOddShortcut);
+Console.WriteLine("\nzadanie3.3");
+
+StreamReader streamReader2 = new StreamReader("skrot2_przyklad.txt");
+
+string strNumberFromFile2;
+List<string> strNumbers2 = new List<string>();
+
+while ((strNumberFromFile2 = streamReader2.ReadLine()) != null)
+{
+    strNumbers2.Add(strNumberFromFile2);
+}
+
+streamReader.Close();
+
+List<int> intNumbers2 = strNumbers2.Select(s => int.Parse(s)).ToList();
+
+int NWD(int a, int b)
+{ 
+    while(b != 0)
+    {
+        int tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+var resoultOfNWD = intNumbers2.Where(n=> oddShortcut(n) > 0 && NWD(n, oddShortcut(n)) == 7).ToList();
+foreach(int r in resoultOfNWD)
+{
+    Console.WriteLine(r);
 }
 
