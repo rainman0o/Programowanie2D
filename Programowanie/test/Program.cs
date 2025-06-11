6bcfa6e67c6e24c3b63ce31c2a088c1c0872e7c7
rainman0o <kupismarcel07@gmail.com>
2025-06-11 19:42:37 +0200

    testy

--- Programowanie/test/Program.cs
+++ Programowanie/test/Program.cs
@@ -1,12 +1,39 @@
-﻿int a = 0;
+﻿using System.Globalization;
 
-Console.WriteLine(a);
-int b = 2;
+List<float> numbers = new List<float>();
 
-int c = a + b;
-Console.WriteLine(c);
 
-if(a + b % 2 == 0)
+Random random = new Random();
+for(int i = 1; i <= 10; i++)
 {
-    Console.WriteLine(c);
+    numbers.Add(random.Next(i , 50));
 }
+
+foreach(float number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine(Math.Pow((numbers[3]), 2));
+
+Console.WriteLine(Math.Sqrt(numbers[5]));
+
+Console.WriteLine("Podaj liczbe do dodania");
+numbers.Add( int.Parse(Console.ReadLine()));
+
+Console.WriteLine(numbers.Last());
+
+var numbersEven = numbers.Select(s => s).Where(s => s % 2 == 0).OrderByDescending(s => s);
+
+foreach(int eaven in numbersEven)
+{
+    Console.WriteLine(eaven);
+}
+
+for(int i = 0;i <= numbers.Count(); i+= 2)
+{
+    Console.WriteLine($"tyle wynosi {i} element {numbers[i]}");
+}
+
+var maxNum = numbers.Max();
+Console.WriteLine($"Tyle wynosi maxymalna liczba{maxNum}");
